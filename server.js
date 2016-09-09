7e573809a8c4d93152ae2c69a960438466409c4f
wmaillard <wmaillard@gmail.com>
2016-09-09 11:48:54 -0700

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
+  socket.on('clientEntities', () => {
+  	io.emit('ping', 'client ' + socket.id + ' just sent me something')
+  })
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
