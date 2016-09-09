76f64189d0b8d3526d77d229d51a1ce5fcb0653b
wmaillard <wmaillard@gmail.com>
2016-09-09 11:57:29 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,12 +13,24 @@ const server = express()
 
 const io = socketIO(server);
 
+var allEntities = [];
+var userEntities;
+
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
-  socket.on('clientEntities', () => {
+  socket.on('clientEntities', (entities) => {
+  	userEntities[socket.id] = entities;
   	io.emit('ping', 'client ' + socket.id + ' just sent me something')
   })
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+setInterval(() => {
+	allEntities = [];
+	for(var userId in userEntities){
+		allEntities = allEntites.concat(userEntities[userId]);
+	}
+	
+	io.emit('time', allEntities)
+
+}, 1000);
