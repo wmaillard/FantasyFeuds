1aad3e07bda20439103c00554d2f0d636bfe698e
wmaillard <wmaillard@gmail.com>
2016-09-13 15:06:59 -0700

    Create globals.js

---
+++ public/js/globals.js
@@ -0,0 +1,45 @@
+//Globals
+
+var APIURL = "https://got-rts.appspot.com";
+var baseNHealth = 1000;
+var baseSHealth = 1000;  //this will be moved when bases are turned into entities
+var blockingTerrain = [];  //Things that you can't walk over
+var characterImages = {};
+var clearBackground = false;  //Should we clear the map
+var click = true;  //Was it a quick click?
+var ctxB;  //Foreground, background, and info context
+var ctxF;
+var ctxI;
+var debugPathfinding = false;
+var entities = [];
+var entitiesMap = []; //nodes that all the entities are on, currently only 15kB
+var entitySpeed = fps * 2 / 5; // Walking speed of entities, probably change this at some point
+var firstLoad = true;  //If true then all the images will be loaded up and cached
+var fps = 5//This is just for panning
+var fullOnPanning = false;  //Is the mouse held down and has it moved over 5px?
+var level = 0;
+var levelHeight;
+var levels = ['theNeck', 'theNorth', 'dorne'];
+var levelsWon = ['theNeck'];  //put this into player probably or just store server side eventually
+var levelTitles = {theNeck: 'The Neck', theNorth: 'The North', dorne: 'Dorne'}
+var levelWidth;
+var mapInterval = false;  //Has the main drawing interval been set?
+var panning = false;  //Is the mouse held down?
+var player1 = {};
+var player2 = {};
+var playerColor = getRandomColor();
+var playerId;
+var serverSentChange = false;
+var size = 32;  //Get rid of this magic number
+var size = 32; //Tile size is 32 x 32, get this from the map so we don't use magic numbers
+var useMin = true; //use minimized images
+var wasCtrl = false;
+var zoom = 1; //starting zoom of map
+var zoomHappened = false;
+
+characterImages.blank.src = 'img/characters/blank.png';
+characterImages.blank = new Image();
+characterImages.giant.src = 'img/characters/giant.png';
+characterImages.giant = new Image();
+characterImages.soldier.src = 'img/characters/soldier.png';
+characterImages.soldier = new Image();
