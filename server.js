dfb55527bd74a4a6ee392de59738007b1023cbf1
wmaillard <wmaillard@gmail.com>
2016-10-18 15:04:21 -0700

    Update server.js

--- server.js
+++ server.js
@@ -64,21 +64,15 @@ var q = 'tasks';
 var url = process.env.CLOUDAMQP_URL || "amqp://localhost";
 var open = require('amqplib').connect(url);
 
-// Consumer
+// Publisher
 open.then(function(conn) {
   var ok = conn.createChannel();
   ok = ok.then(function(ch) {
     ch.assertQueue(q);
-    ch.consume(q, function(msg) {
-      if (msg !== null) {
-        console.log(msg.content.toString());
-        ch.ack(msg);
-      }
-    });
+    ch.sendToQueue(q, new Buffer('something to do'));
   });
   return ok;
 }).then(null, console.warn);
-
 
 
 
