8206b94847cc560649bbf6b670618b4d264a9dd8
wmaillard <wmaillard@gmail.com>
2016-10-18 12:34:47 -0700

    Update aiserver.js

--- aiserver.js
+++ aiserver.js
@@ -39,7 +39,7 @@ setTimeout(function(){
 			y: ~~(data.endY / 32)
 		}, blockingTerrain);*/
 
-		pathSocket.of('/path').emit('path', { some: 'data***********' });
+		pathSocket.emit('path', { some: 'data***********' });
 
 		})
 	});
