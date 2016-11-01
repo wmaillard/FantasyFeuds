bdf3595370bd06d663f759c9025f60e3541e04fa
wmaillard <maillarw@oregonstate.edu>
2016-11-01 19:55:24 +0000

    fixed grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,12 @@
 module.exports = function(grunt) {
-
+  var path = 'public/js/src/';
+  var files = ['blockingTerrain.js', 'generalUtilities.js', 'drawingUtilities.js', 'entityUtilities.js', 'castles.js',
+  'entityInfo.js', 'globals.js', 'mapPointUtilities.js',
+  'controlFunctions.js', 'drawMap.js', 'drawEntities.js', 'bindButtons.js', 'entities.js',  
+  'hammerSetup.js', 'setUpSocketListeners.js', 'playerControls.js', 'main.js'];
+  for(var i = 0; i < files.length; i++){
+    files[i] =  path + files[i];
+  }
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -7,10 +14,11 @@ module.exports = function(grunt) {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
-      build: {
-        src: 'public/js/src/*.js',
-        dest: 'public/js/build/*.min.js'
-      }
+     my_target: {
+          files: {
+            'public/js/build/game.min.js': files
+          }
+    }
     }
   });
 
