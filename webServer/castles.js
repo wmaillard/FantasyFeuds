8d879f656de9fae469c19c3d07fd52e901a4a6f3
Will Maillard <wmaillard@gmail.com>
2016-11-04 10:04:57 -0700

    Update castles.js

--- webServer/castles.js
+++ webServer/castles.js
@@ -102,8 +102,6 @@ var castles = {
         }
     },
     canAddHere : function(e) {
-        console.log(JSON.stringify(this.castles));
-        console.log(e.team);
         var castles = this.castles;
         var rx = this.castleRadius / 2.5;
         var ry = this.castleRadius / 3;
