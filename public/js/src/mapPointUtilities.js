2e35b53a9e4b689a5a9fe893c0abc250af95d0b8
Will Maillard <wmaillard@gmail.com>
2016-11-04 13:04:59 -0700

    Update mapPointUtilities.js

--- public/js/src/mapPointUtilities.js
+++ public/js/src/mapPointUtilities.js
@@ -2,7 +2,8 @@
 
 function isInWindow(x, y, width, height){
     if(width && height){
-      return isInWindow(x, y) || isInWindow(x + width, y) || isInWindow(x + width, y + height) || isInWindow(x, y + height);
+        // x, y is the center of the box
+      return isInWindow(x - width / 2, y - height / 2) || isInWindow(x + width / 2, y - height / 2) || isInWindow(x + width / 2, y + height / 2) || isInWindow(x - width / 2, y + height / 2);
     }
     if(x + backgroundOffset.x < canvasWidth / zoom && y + backgroundOffset.y < canvasHeight / zoom && x + backgroundOffset.x >= 0 &&  y + backgroundOffset.y >= 0){
         return true;
