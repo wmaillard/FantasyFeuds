cdf9c0be52f5c324ef4c513d1de02fc8d9e275f8
Will Maillard <wmaillard@gmail.com>
2016-11-07 11:23:24 -0800

    Update webServer.js

--- webServer/webServer.js
+++ webServer/webServer.js
@@ -164,7 +164,7 @@ function runServer() {
                                     delete walkingEntities[i];
                                 }
                             }
-                        } else {
+                        } else if(changes[i]) {  //changes[i] is sometimes undefined, BUG
                             entities[i] = changes[i];
                             entities[i].walking = false;  //Start as walking then kill, gives a nice effect but not necessary
                         }
