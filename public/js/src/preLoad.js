fc27efaedbd8a0f39a8d5b0083f56fbfc40d2219
Will Maillard <wmaillard@gmail.com>
2016-11-23 09:16:38 -0800

    Create preLoad.js

---
+++ public/js/src/preLoad.js
@@ -0,0 +1,37 @@
+ Pace.on('done', function() {
+        $('#closeIntro').click(function(e) {
+            if($('#skipTutorial').is(':checked')){
+                for(var i in firstTime){
+                    firstTime[i] = false;
+                }
+            }else{
+                    $('#showShop').addClass('breathing');
+            }
+            if ($('#screenName').val() === "") {
+                $("#screenName").fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+            } else {
+                name = $('#screenName').val();
+                socket.emit('name', name);
+                $('#startInfo').toggle();
+                $('#introTeamBox').toggle();
+                setTimeout(function() {
+                    $('#introTeamBox').fadeOut('slow');
+                }, 1000)
+            };
+        });
+        $('#closeIntro').removeClass('disabled');
+    });
+    function runTips(i) {
+    if ($('#startInfo').is(":visible")) {
+        setTimeout(function() {
+            $('#didYouKnow').fadeTo('slow', .01, function() {
+                $('#didYouKnow').text(tips[i]);
+                $('#didYouKnow').fadeTo( 'slow', 1);
+                i++;
+                i %= tips.length;
+                runTips(i);
+            })
+        }, 3000);
+    }
+}
+runTips(0)
