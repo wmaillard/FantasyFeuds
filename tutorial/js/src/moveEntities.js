4f25b5800fe761098e48b401a05cb932d6149bd8
Will <wmaillard@gmail.com>
2016-12-29 20:16:41 -0800

    href

--- tutorial/js/src/moveEntities.js
+++ tutorial/js/src/moveEntities.js
@@ -156,7 +156,7 @@ var moveEntities = { //Currently mutates entities
 											$('#tutorialModalButton').text('Play Now!').unbind( "click" ).click(function(){
 												
 												$('body').empty().append('<div class="row"><div class="col-xs-10 offset-xs-1"><div class="spinner"><div class="cube1"></div><div class="cube2"></div></div></div></div>')
-												 window.location = 'http://fantasyfeuds.com';
+												 window.location = 'http://fantasyfeuds.com/game';
 											})
 									});
 									
